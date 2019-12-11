f2f2e51659179c5886b20e63a9514dc1e93dfdc4
Ipe92 <Ipe92@users.noreply.github.com>
2019-12-11 08:14:42 +0200

    11/12/2019 koodit. btw aho teki 2 maalia.

---
+++ chapter_7_11_quiz.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+void printBinary(int x)
+{
+	// Termination case
+	if (x == 0)
+		return;
+
+	// Recurse to the next bit
+	printBinary(x / 2);
+
+	// Print out the remainders (in reverse order)
+	std::cout << x % 2;
+}
+
+int main()
+{
+	int x;
+	std::cout << "Enter positive integer: ";
+	std::cin >> x;
+
+	printBinary(x);
+
+	return 0;
+}
